8f9658aaa0dd43eda5a4330c6bbb765c69b5f598
ArtemMaxnev <artem_maxnev124456@mail.ru>
2020-06-27 01:42:12 +0500

    Update DBUtils.cs

--- SalaryApp/SalaryApp/DBUtils.cs
+++ SalaryApp/SalaryApp/DBUtils.cs
@@ -8,9 +8,9 @@ namespace SalaryApp
         {
             string host = "91.191.231.76";
             int port = 3307;
-            string database = "pr_is207_18";
-            string username = "pr_is207_18";
-            string password = "pr_is207_18";
+            string database = "pr_is207_14";
+            string username = "pr_is207_14";
+            string password = "pr_is207_14";
 
             return DBMySQLUtils.GetDBConnection(host, port, database, username, password);
         }
